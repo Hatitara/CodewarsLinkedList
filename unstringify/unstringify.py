da4f43d823b9f4db8a0a4b3f1e1970a7cd343baf
Yaroslav Kornetskyi <kornetskyi.pn@ucu.edu.ua>
2024-04-03 10:25:26 +0300

    Simplified, made better, added proof

--- unstringify/unstringify.py
+++ unstringify/unstringify.py
@@ -14,20 +14,10 @@ def linked_list_from_string(s: str) -> 'Node':
     '''
     unstringifier
     '''
-    storage = [int(k) if all(c.isdigit() for c in k) else None \
-               for k in s.split(' -> ')]
-    bad = [k for k in storage if k is not None and k < 0]
-    if storage[0] is None or not storage or bad:
-        return None
-    res = Node(None)
-    current_node = res
-    for pos, element in enumerate(storage):
-        current_node.data = element
-        if pos < len(storage) and storage[pos + 1] is None:
-            break
-        current_node.next = Node(None)
-        current_node = current_node.next
-    return res if res.data != 'None' and res.data is not None else 'None'
+    current_node = None
+    for element in s.split(' -> ')[-2::-1]:
+        current_node = Node(data=int(element), next=current_node)
+    return current_node
 
 if __name__ == '__main__':
     x = linked_list_from_string("0 -> 1 -> 4 -> 9 -> 16 -> None")
